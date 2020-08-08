129b47578a8c7e554e51526db969f65db0bf401a
airflow-workstation <ec2-user@ip-172-31-18-152.eu-west-3.compute.internal>
2020-08-08 18:55:01 +0000

    upgrade integration tests

--- integrationtests/test_integration_simple_pipe.py
+++ integrationtests/test_integration_simple_pipe.py
@@ -1,36 +1,69 @@
+from airflow.api.common.experimental import trigger_dag as trigger
+from airflow.api.common.experimental import delete_dag as delete
+from airflow.api.common.experimental.get_dag_run_state import get_dag_run_state
+from airflow.utils import timezone
+from airflow.exceptions import AirflowException
+from airflow import models
+
 import pytest
-import requests
 import json
 import os
 
 class TestIntegrationSimplePipe:
 
-    AIRFLOW_URL = "http://localhost:8080"
-
     def pause_dag(self, dag_id, pause):
-        requests.get("{}/api/experimental/dags/{}/paused/{}".format(self.AIRFLOW_URL, dag_id, pause))
-        status = json.loads(requests.get("{}/api/experimental/dags/{}/paused".format(self.AIRFLOW_URL, dag_id)).content)["is_paused"]
-        return status
+        """(Un)pauses a dag"""
+        models.DagModel.get_dagmodel(dag_id).set_is_paused(
+            is_paused=pause
+        )
+        
+    def clean_dag(self, dag_id):
+        """
+        Delete all DB records related to the specified Dag.
+        """
+        try:
+            count = delete.delete_dag(dag_id)
+        except:
+            pass
+        
+    def trigger_dag(self, dag_id, execution_date):
+        """
+        Trigger a new dag run for a Dag with an execution date.
+        """        
+        execution_date = timezone.parse(execution_date)
+        trigger.trigger_dag(dag_id, None, None, execution_date)
 
+    def status_dag(self, dag_id, execution_date):
+        """
+        Get the status of a given DagRun according to the execution date
+        """
+        execution_date = timezone.parse(execution_date)
+        info = get_dag_run_state(dag_id, execution_date)
+        return json.loads(info)
+        
     def test_simple_pipe(self):
         """ Simple Pipe should run successfully """
         execution_date = "2020-05-21T12:00:00+00:00"
         dag_id = "simple_pipe"
-        headers = {'Cache-Control': 'no-cache', 'Content-Type': 'application/json'}
-        data = '{"execution_date": "%s"' % (execution_date)
+
+        # Delete data related to the DAG (if already exists)
+        self.clean_dag(dag_id)
         
-        # Unpause DAG - Required to trigger it even manually
-        assert self.pause_dag(dag_id, "false") == False, "The DAG {} is still in pause".format(dag_id)
+        # Unpause DAG - Required to trigger it, even manually
+        self.pause_dag(dag_id, False)
 
         # Trigger the DAG
-        if requests.post("{}/api/experimental/dags/{}/dag_runs".format(self.AIRFLOW_URL, dag_id), headers=headers, data=data).status_code != 200:
-            raise Exception("Can't reach the API of Airflow")
+        self.trigger_dag(dag_id, execution_date)
         
         # Check if it is running as expected
         is_running = True
         while is_running:
-            is_running = json.loads(requests.get("{}/api/experimental/dags/{}/dag_runs/{}".format(self.AIRFLOW_URL, dag_id, execution_date)).content)["status"]
+            dagrun = self.status_dag(dag_id, execution_date)['state']
+            if dagrun['state'] not in ['running', 'success']:
+                return
+            if dagrun['state'] == 'success':
+                is_running = False
         assert is_running == False, "The DAG {} didn't run as expected".format(dag_id)
 
-        # pause DAG - Required to trigger it even manually
-        assert self.pause_dag(dag_id, "true") == True, "The DAG {} did get paused as expected".format(dag_id)
+        # pause DAG
+        self.pause_dag(dag_id, True)
